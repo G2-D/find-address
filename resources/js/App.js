7a0f4011ebee815929ed9e087727708ec6bc0bbe
José Carlos <j.de.oliveira.lima@avanade.com>
2020-08-29 14:21:01 -0300

    correction path

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,5 +1,5 @@
 // Style
-import "../sass/App.scss";
+import "../sass/app.scss";
 
 // FormPostalCode
 import FormPostalCode from "./components/FormPostalCode";
@@ -7,23 +7,21 @@ import FormPostalCode from "./components/FormPostalCode";
 /**
  * Form 1
  */
-const form_1 = document.getElementById('form_1');
+const form_1 = document.getElementById("form_1");
 
- if (form_1) {
+if (form_1) {
+  const formPostalCode = new FormPostalCode("#form_1");
 
-	 const formPostalCode = new FormPostalCode('#form_1');
-	 
-	 formPostalCode.create();
- }
+  formPostalCode.create();
+}
 
 /**
  * Form 2
  */
-const form_2 = document.getElementById('form_2');
+const form_2 = document.getElementById("form_2");
 
 if (form_2) {
+  const formPostalCode2 = new FormPostalCode(form_2);
 
-	const formPostalCode2 = new FormPostalCode(form_2);
-	
-	formPostalCode2.create();
+  formPostalCode2.create();
 }
