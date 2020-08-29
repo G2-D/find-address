0a972e74b865849267844363fba59c1cf0566943
José Carlos <j.de.oliveira.lima@avanade.com>
2020-08-29 13:17:37 -0300

    create form and apply itau brand

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,14 +1,14 @@
 // Style
-import '../sass/App.scss';
+import "../sass/App.scss";
 
 // FormPostalCode
-import FormPostalCode from './components/FormPostalCode';
+import FormPostalCode from "./components/FormPostalCode";
 
-const form = document.getElementById('form_postal_code');
+// const form = document.getElementById('form_postal_code');
 
-const formPostalCode = new FormPostalCode(form);
+// const formPostalCode = new FormPostalCode(form);
 
-formPostalCode.configure({
-	action 	: 'http://api.postmon.com.br/v1/cep',
-	method	: 'GET'
-}).create();
+// formPostalCode.configure({
+// 	action 	: 'http://api.postmon.com.br/v1/cep',
+// 	method	: 'GET'
+// }).create();
